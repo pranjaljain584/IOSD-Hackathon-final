aa18e8c102b5d9bef2bce390fbb800009f92c0da
Granth Verma <granthverma@Granths-MacBook-Air.local>
2021-03-09 00:45:09 +0530

    teacher leaderboard error rectified

--- frontend/src/views/TableList/TableList.js
+++ frontend/src/views/TableList/TableList.js
@@ -1,15 +1,42 @@
 import React, {Component} from 'react';
 import Table2 from '../../components/Table/Table2'
 import { connect } from "react-redux";
+import axios from "axios";
 class TableList extends Component {
-
+    state={
+        classes:[]
+    };
+    componentDidMount(){
+        const config = {
+            headers: {
+              "Content-Type": "application/json",
+              "x-auth-token": localStorage.token,
+            },
+          }
+        if(this.props.auth.isStudent)
+    {
+      axios.get("http://localhost:5000/api/user/joinedClasses",config)
+          .then(response=>{
+            console.log(response.data);
+            this.setState({classes:response.data});
+          })
+    }
+    else
+    {
+      axios.get("http://localhost:5000/api/teacher/myClassrooms",config)
+          .then(response=>{
+            console.log(response.data);
+            this.setState({classes:response.data});
+          })
+    }
+    }
     render() {
         console.log("%%%%", this.props.auth.user.classes)
         return (
 
             <div style={{display: 'flex', justifyContent: "center", alignItem: 'center',flexDirection:"column"}}>
-                {this.props.auth.user.classes.map((classId)=>(
-                    <Table2  classId={classId}/>
+                {this.state.classes.map((classId)=>(
+                    <Table2  classId={classId._id}/>
                 ))}
 
             </div>

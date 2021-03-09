84081b9a0551fa135efb9a0c9a47dd33b4ec2192
Kharanshu31 <dewankharanshu@gmail.com>
2021-03-09 23:45:15 +0530

    Update student response

--- backend/routes/api/studentresponse.js
+++ backend/routes/api/studentresponse.js
@@ -4,6 +4,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const Studentresponse = require('../../models/studentresponse');
+const User = require("../../models/User");
 
 router.post('/',
   [
@@ -38,7 +39,30 @@ router.post('/',
 router.get('/:id',auth,async(req,res)=>{
   try {
     const s = await Studentresponse.find({assignmentid:req.params.id});
-    res.json(s);
+    //res.json(s);
+    let ar=[];
+    //console.log(' responses ',s);
+    for(let i=0;i<s.length;i++)
+    {
+      const u=await User.findOne({_id:s[i].user});
+      //console.log('user ',u);
+      if(u)
+      {
+        ar.push({
+          text:s[i].text,
+          name:u.name
+        })
+      }
+      else
+      {
+        ar.push({
+          text:s[i].text,
+          name:"No name"
+        })
+      }
+    }
+
+    res.json(ar);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');

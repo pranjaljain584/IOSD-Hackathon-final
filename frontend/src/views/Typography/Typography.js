83c089ac3421d6cf70023170b20a9268786b759a
Aasritha Lingam <lingamaasritha@gmail.com>
2021-03-09 13:27:32 +0400

    typography 1

--- frontend/src/views/Typography/Typography.js
+++ frontend/src/views/Typography/Typography.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {CanvasJSChart} from 'canvasjs-react-charts'
 // @material-ui/core components
 // import { makeStyles } from "@material-ui/core/styles";
 // core components
@@ -12,9 +13,53 @@ import React from "react";
 // import Card from "components/Card/Card.js";
 // import CardHeader from "components/Card/CardHeader.js";
 // import CardBody from "components/Card/CardBody.js";
+var Component = React.Component;
+var CanvasJSReact = require('./canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 
 export default function TypographyPage() {
+  const options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2", //"light1", "dark1", "dark2"
+    title:{
+      text: "Your Activity over the Past week"
+    },
+    axisY: {
+      includeZero: true
+    },
+    data: [{
+      type: "column", //change type to bar, line, area, pie, etc
+      //indexLabel: "{y}", //Shows y value on all Data Points
+      indexLabelFontColor: "#5A5757",
+      indexLabelPlacement: "outside",
+      dataPoints: [
+        { x: 10, y: 71 },
+        { x: 20, y: 55 },
+        { x: 30, y: 50 },
+        { x: 40, y: 65 },
+        { x: 50, y: 71 },
+        { x: 60, y: 68 },
+        { x: 70, y: 38 },
+        { x: 80, y: 92, indexLabel: "Highest" },
+        { x: 90, y: 54 },
+        { x: 100, y: 60 },
+        { x: 110, y: 21 },
+        { x: 120, y: 49 },
+        { x: 130, y: 36 }
+      ]
+    }]
+  }
+  
   return (
-    <div></div>
+    <div>
+      <CanvasJSChart options = {options} 
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+    
   );
 }
